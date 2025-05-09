2eb45c82aebda35f28327f161b4877284d27f0d7
neinja007 <anton.siligan@gmail.com>
2025-05-09 16:29:43 +0200

    wrap app in clerk provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const geistSans = Geist({
 	subsets: ['latin']
@@ -22,10 +23,12 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en'>
-			<body className={`${geistSans.className} ${geistMono.variable} antialiased`}>
-				<div className='h-screen w-full max-w-screen-2xl mx-auto pb-16'>{children}</div>
-			</body>
-		</html>
+		<ClerkProvider>
+			<html lang='en'>
+				<body className={`${geistSans.className} ${geistMono.variable} antialiased`}>
+					<div className='h-screen w-full max-w-screen-2xl mx-auto pb-16'>{children}</div>
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
