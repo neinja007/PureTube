1bf19bbcd56a3ca4269e6eb13160d510b1956d95
neinja007 <anton.siligan@gmail.com>
2025-05-09 16:33:01 +0200

    add size prop to logo component

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,17 @@
 import Image from 'next/image';
 
-const Logo = () => {
+const Logo = ({ size = 'md' }: { size?: 'sm' | 'md' | 'lg' }) => {
 	return (
-		<div className='flex items-center gap-2'>
-			<Image src='/icon.png' alt='PureTube' width={50} height={50} />
-			<span className='text-4xl font-bold'>PureTube</span>
+		<div className={`flex items-center ${size === 'sm' ? 'gap-2' : size === 'md' ? 'gap-3' : 'gap-4'}`}>
+			<Image
+				src='/icon.png'
+				alt='PureTube'
+				width={size === 'sm' ? 24 : size === 'md' ? 32 : 75}
+				height={size === 'sm' ? 24 : size === 'md' ? 32 : 75}
+			/>
+			<span className={`font-bold ${size === 'sm' ? 'text-2xl' : size === 'md' ? 'text-3xl' : 'text-6xl'}`}>
+				PureTube
+			</span>
 		</div>
 	);
 };
