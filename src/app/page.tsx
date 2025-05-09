ae34ed60e06370be6d911cd8fc6768f806ad4389
neinja007 <anton.siligan@gmail.com>
2025-05-09 16:33:06 +0200

    display large logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Logo from '@/components/logo';
 
 export default function Home() {
 	return (
-		<div className='flex flex-col items-center justify-center h-screen'>
-			<Logo />
-			<p className='text-sm text-gray-500'>YouTube, but without the junk.</p>
+		<div className='flex flex-col items-center justify-center mt-32'>
+			<Logo size='lg' />
+			<p className='text-xl text-gray-500'>YouTube, but without the junk.</p>
 		</div>
 	);
 }
