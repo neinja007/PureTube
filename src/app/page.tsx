5c27d57930923d103aced0b2f1451d70bd31bfc7
neinja007 <anton.siligan@gmail.com>
2025-05-10 11:03:09 +0200

    add sign in button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Logo from '@/components/logo';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
 	return (
 		<div className='flex flex-col items-center justify-center mt-32'>
 			<Logo size='lg' />
 			<p className='text-xl text-gray-500'>YouTube, but without the junk.</p>
+			<Button size='lg' className='mt-4'>
+				Sign in to get started
+			</Button>
 		</div>
 	);
 }
