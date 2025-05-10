b69cf40245b60bea45920edcb9303372292f4c8f
neinja007 <anton.siligan@gmail.com>
2025-05-10 10:57:44 +0200

    remove border

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import Logo from './logo';
 
 const Navbar = () => {
 	return (
-		<div className='h-16 bg-purple-200 border-gray-300 border-b w-full sticky top-0 flex justify-between items-center px-4'>
+		<div className='h-16 bg-purple-200 w-full sticky top-0 flex justify-between items-center px-4'>
 			<Link href='/'>
 				<Logo size='md' />
 			</Link>
