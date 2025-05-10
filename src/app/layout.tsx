524d40d1d59dd9c52948691e74bda0061ced0373
neinja007 <anton.siligan@gmail.com>
2025-05-10 12:05:45 +0200

    wrap in query client provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import Navbar from '@/components/navbar';
+import { QueryProvider } from '@/components/query-provider';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const geistSans = Geist({
 	subsets: ['latin']
@@ -25,12 +27,15 @@ export default function RootLayout({
 }>) {
 	return (
 		<ClerkProvider>
-			<html lang='en'>
-				<body className={`${geistSans.className} ${geistMono.variable} antialiased`}>
-					<Navbar />
-					<div className='w-full max-w-screen-2xl mx-auto pb-16'>{children}</div>
-				</body>
-			</html>
+			<QueryProvider>
+				<html lang='en'>
+					<body className={`${geistSans.className} ${geistMono.variable} antialiased`}>
+						<Navbar />
+						<div className='w-full max-w-screen-2xl mx-auto pb-16'>{children}</div>
+					</body>
+				</html>
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryProvider>
 		</ClerkProvider>
 	);
 }
