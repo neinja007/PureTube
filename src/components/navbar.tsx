33ecd317d04bc73715c1b871d10aecaef3fd81d8
neinja007 <anton.siligan@gmail.com>
2025-05-10 12:00:51 +0200

    replace home links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,12 +7,10 @@ import { SignedIn, SignedOut, useUser } from '@clerk/nextjs';
 const Navbar = () => {
 	const { user } = useUser();
 
-	console.log(user);
-
 	return (
 		<div className='bg-purple-200 w-full sticky border-b border-gray-400 top-0 flex justify-between items-center px-5'>
 			<div className='flex items-center gap-5'>
-				<Link href={user ? '/home' : '/'} className='py-4 border-r border-gray-400 pr-5'>
+				<Link href={user ? '/profile/default' : '/'} className='py-4 border-r border-gray-400 pr-5'>
 					<Logo size='md' />
 				</Link>
 				<SignedOut>
@@ -26,11 +24,7 @@ const Navbar = () => {
 					</div>
 				</SignedOut>
 				<SignedIn>
-					<div className='flex gap-5 items-center'>
-						<Link href='/home' className='text-black'>
-							Home
-						</Link>
-					</div>
+					<div className='flex gap-5 items-center'></div>
 				</SignedIn>
 			</div>
 			<div>
