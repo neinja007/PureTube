cc9e983002b4b5ff5efda4cc08e70d752203816f
neinja007 <anton.siligan@gmail.com>
2025-05-10 11:12:03 +0200

    add buttons to sign up and sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
 import Logo from '@/components/logo';
 import { Button } from '@/components/ui/button';
+import { SignInButton, SignUpButton } from '@clerk/nextjs';
 
 export default function Home() {
 	return (
-		<div className='flex flex-col items-center justify-center mt-32'>
+		<div className='flex flex-col items-center justify-center mt-16'>
 			<Logo size='lg' />
 			<p className='text-xl text-gray-500'>YouTube, but without the junk.</p>
-			<Button size='lg' className='mt-4'>
-				Sign in to get started
-			</Button>
+			<div className='mt-4 flex gap-2 items-center'>
+				<SignUpButton>
+					<Button>Sign up to get started</Button>
+				</SignUpButton>
+				or
+				<SignInButton>
+					<Button variant='outline'>Sign in to your account</Button>
+				</SignInButton>
+			</div>
 		</div>
 	);
 }
