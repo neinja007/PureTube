f569a58234dd5a74c1623c773c6430d83412367f
neinja007 <anton.siligan@gmail.com>
2025-05-10 11:15:11 +0200

    redirect not logged in users to home

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,15 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, req) => {
+	const { userId } = await auth();
+
+	if (!userId && isProtectedRoute(req)) {
+		return NextResponse.redirect(new URL('/', req.url));
+	}
+});
+
+const isProtectedRoute = createRouteMatcher(['/((?!api|trpc|auth/sign-in|auth/sign-up|$).*)']);
 
 export const config = {
 	matcher: [
