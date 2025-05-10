4f189350c792aa7bdc62cb28af8ba34e27347972
neinja007 <anton.siligan@gmail.com>
2025-05-10 12:00:06 +0200

    add query provider component

---
+++ src/components/query-provider.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useEffect, useState } from 'react';
+import { persistQueryClient } from '@tanstack/react-query-persist-client';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+
+export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: {
+						staleTime: 5 * 60 * 1000
+					}
+				}
+			})
+	);
+
+	useEffect(() => {
+		const persister = createSyncStoragePersister({
+			storage: window.localStorage
+		});
+
+		persistQueryClient({
+			queryClient,
+			persister
+		});
+	}, [queryClient]);
+
+	queryClient.setQueryDefaults(['dbUser'], {
+		staleTime: 0
+	});
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			{children}
+			<ReactQueryDevtools initialIsOpen={true} />
+		</QueryClientProvider>
+	);
+};
