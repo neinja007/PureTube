79c12f7918e4ac68a562e57a89ae46e3438279de
neinja007 <anton.siligan@gmail.com>
2025-05-11 09:46:17 +0200

    add basic stuff

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,5 +1,22 @@
-const Page = () => {
-	return <div>Page</div>;
+import { Button } from '@/components/ui/button';
+import { Settings } from 'lucide-react';
+import Link from 'next/link';
+
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+	const { id } = await params;
+
+	return (
+		<div className='flex flex-col gap-4 mt-10'>
+			<h1 className='text-2xl font-bold flex justify-between items-center'>
+				Default Profile
+				<Link href={`/profile/${id}/edit`}>
+					<Button variant='ghost' size='icon'>
+						<Settings className='size-5' />
+					</Button>
+				</Link>
+			</h1>
+		</div>
+	);
 };
 
 export default Page;
