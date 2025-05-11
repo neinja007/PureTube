cb8fe690801e9d02a1e8f378b2e57282f3d9f9bf
neinja007 <anton.siligan@gmail.com>
2025-05-11 10:49:51 +0200

    include subs in response type

--- src/hooks/use-youtube-channels.ts
+++ src/hooks/use-youtube-channels.ts
@@ -4,7 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 export const useYoutubeChannels = (query: string) => {
-	return useQuery<{ id: string; name: string; thumbnail: string }[]>({
+	return useQuery<{ id: string; name: string; thumbnail: string; subscribers: string }[]>({
 		queryKey: ['youtube-channels', query],
 		queryFn: () => axios.get(`/api/youtube/channels?query=${query}`).then((res) => res.data)
 	});
