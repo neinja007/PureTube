0c772132e6da33d712ae94f13312a2f532e695ed
neinja007 <anton.siligan@gmail.com>
2025-05-11 14:45:18 +0200

    disable if no query is present

--- src/hooks/use-youtube-channels.ts
+++ src/hooks/use-youtube-channels.ts
@@ -6,6 +6,7 @@ import axios from 'axios';
 export const useYoutubeChannels = (query: string) => {
 	return useQuery<{ id: string; name: string; thumbnail: string; subscribers: string }[]>({
 		queryKey: ['youtube-channels', query],
-		queryFn: () => axios.get(`/api/youtube/channels?query=${query}`).then((res) => res.data)
+		queryFn: () => axios.get(`/api/youtube/channels?query=${query}`).then((res) => res.data),
+		enabled: query.length > 0
 	});
 };
