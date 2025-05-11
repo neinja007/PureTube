8c9519b0c5c6c842019fc06af9881debd6403dd9
neinja007 <anton.siligan@gmail.com>
2025-05-11 14:45:05 +0200

    move editing to collection page into dialog

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -4,16 +4,19 @@ import { Button } from '@/components/ui/button';
 import { useCollection } from '@/hooks/collection/use-collection';
 import { useUser } from '@clerk/nextjs';
 import { Settings } from 'lucide-react';
-import Link from 'next/link';
 import { useParams, useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { ChannelList } from '@/components/channels/channel-list';
+import { ChannelSearch } from '@/components/channels/channel-search';
 
 const Page = () => {
 	const params = useParams();
 	const id = params.id as string;
+	const [isOpen, setIsOpen] = useState(false);
+	const [channels, setChannels] = useState<string[]>([]);
 
 	const { user } = useUser();
-
 	const { replace } = useRouter();
 
 	useEffect(() => {
@@ -22,17 +25,32 @@ const Page = () => {
 		}
 	}, [user, replace, id]);
 
-	const { data: collection } = useCollection(id || '');
+	const { data: collection, status } = useCollection(id || '');
+
+	useEffect(() => {
+		if (collection?.channels) {
+			setChannels(collection.channels.map((channel) => channel.name));
+		}
+	}, [collection]);
 
 	return (
 		<div className='flex flex-col gap-4 mt-10'>
 			<h1 className='text-2xl font-bold flex justify-between items-center'>
 				{collection?.name}
-				<Link href={`/profile/${id}/edit`}>
-					<Button variant='ghost' size='icon'>
-						<Settings className='size-5' />
-					</Button>
-				</Link>
+				<Dialog open={isOpen} onOpenChange={setIsOpen}>
+					<DialogTrigger asChild>
+						<Button variant='ghost' size='icon'>
+							<Settings className='size-5' />
+						</Button>
+					</DialogTrigger>
+					<DialogContent className='max-w-3xl'>
+						<DialogHeader>
+							<DialogTitle>Edit {collection?.name}</DialogTitle>
+						</DialogHeader>
+						<ChannelList channels={channels} status={status} />
+						<ChannelSearch onChannelSelect={(channelName) => setChannels([...channels, channelName])} />
+					</DialogContent>
+				</Dialog>
 			</h1>
 			<div className='flex flex-col gap-4'>
 				{collection?.channels.length && collection?.channels.length > 0 ? (
@@ -40,9 +58,9 @@ const Page = () => {
 				) : (
 					<div>
 						No channels found.{' '}
-						<Link href={`/profile/${id}/edit`} className='hover:underline text-blue-500'>
+						<button onClick={() => setIsOpen(true)} className='hover:underline text-blue-500'>
 							You can add some here.
-						</Link>
+						</button>
 					</div>
 				)}
 			</div>
