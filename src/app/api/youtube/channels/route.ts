b2eee798c8566d72a3cb10098dc25f9f6c837bfe
neinja007 <anton.siligan@gmail.com>
2025-05-11 10:49:44 +0200

    include sub count in result

--- src/app/api/youtube/channels/route.ts
+++ src/app/api/youtube/channels/route.ts
@@ -1,6 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 import axios from 'axios';
 
+interface ChannelStatistics {
+	subscriberCount: string;
+	viewCount: string;
+	videoCount: string;
+}
+
 export async function GET(request: NextRequest) {
 	try {
 		const query = request.nextUrl.searchParams.get('query');
@@ -9,22 +15,38 @@ export async function GET(request: NextRequest) {
 			return NextResponse.json({ error: 'Query parameter is required' }, { status: 400 });
 		}
 
-		const response = await axios
+		const searchResponse = await axios
 			.get(
-				`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&results=5&type=channel&key=${process.env.YOUTUBE_API_KEY}`
+				`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&maxResults=5&type=channel&key=${process.env.YOUTUBE_API_KEY}`
 			)
 			.then((res) => res.data);
 
+		const channelIds = searchResponse.items?.map((item: { id: { channelId: string } }) => item.id.channelId) || [];
+
+		const channelsResponse = await axios
+			.get(
+				`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelIds.join(',')}&key=${
+					process.env.YOUTUBE_API_KEY
+				}`
+			)
+			.then((res) => res.data);
+
+		const channelStats = new Map<string, ChannelStatistics>(
+			channelsResponse.items?.map((item: { id: string; statistics: ChannelStatistics }) => [
+				item.id,
+				item.statistics
+			]) || []
+		);
+
 		const channels =
-			response.items?.map(
+			searchResponse.items?.map(
 				(item: {
 					id: { kind: string; channelId: string };
 					snippet: { title: string; thumbnails: { default: { url: string } } };
-					statistics: { subscriberCount: string };
 				}) => ({
 					id: item.id?.channelId,
 					name: item.snippet?.title,
-					subscribers: item.statistics?.subscriberCount,
+					subscribers: channelStats.get(item.id.channelId)?.subscriberCount || '0',
 					thumbnail: item.snippet?.thumbnails?.default?.url
 				})
 			) || [];
