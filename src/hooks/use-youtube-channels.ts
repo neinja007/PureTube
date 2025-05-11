8a4c1cf73b693e572b767f19801a2b4a7276b80a
neinja007 <anton.siligan@gmail.com>
2025-05-11 10:46:25 +0200

    change response type

--- src/hooks/use-youtube-channels.ts
+++ src/hooks/use-youtube-channels.ts
@@ -1,11 +1,10 @@
 'use client';
 
-import { Channel, Collection } from '@prisma/client';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 export const useYoutubeChannels = (query: string) => {
-	return useQuery<Collection & { channels: Channel[] }>({
+	return useQuery<{ id: string; name: string; thumbnail: string }[]>({
 		queryKey: ['youtube-channels', query],
 		queryFn: () => axios.get(`/api/youtube/channels?query=${query}`).then((res) => res.data)
 	});
