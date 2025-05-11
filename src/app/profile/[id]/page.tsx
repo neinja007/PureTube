c077103e43d52aa8c8b76b31adc374e33a0ce61a
neinja007 <anton.siligan@gmail.com>
2025-05-11 10:01:45 +0200

    fix incorrect use of params

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,22 +1,28 @@
-import { CollectionFeed } from '@/components/collection-feed';
+'use client';
+
 import { Button } from '@/components/ui/button';
+import { useCollection } from '@/hooks/collection/use-collection';
 import { Settings } from 'lucide-react';
 import Link from 'next/link';
+import { useParams } from 'next/navigation';
 
-const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
-	const { id } = await params;
+const Page = () => {
+	const params = useParams();
+	const id = params.id as string;
+
+	console.log(id);
+	const { data: collection } = useCollection(id || '');
 
 	return (
 		<div className='flex flex-col gap-4 mt-10'>
 			<h1 className='text-2xl font-bold flex justify-between items-center'>
-				Default Profile
+				{collection?.name}
 				<Link href={`/profile/${id}/edit`}>
 					<Button variant='ghost' size='icon'>
 						<Settings className='size-5' />
 					</Button>
 				</Link>
 			</h1>
-			<CollectionFeed id={id} />
 		</div>
 	);
 };
